1aefc5e10719e5f134900106ca85a66c6e1eca96
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2023-10-14 22:04:04 +0300

    do dz1

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+let content;
+let padding; 
+let border;
+
+function getElementWidth(content, padding, border) {
+    const sizeContent = Number.parseInt(content);
+    const sizePadding = Number.parseInt(padding);
+    const sizeBorder = Number.parseFloat(border);
+    return sizeContent + 2 * sizePadding + 2 * sizeBorder;  
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
